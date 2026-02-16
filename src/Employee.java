27684cc3b23eba62779e098f2ff2e9122f8dd42b
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-02-16 22:40:22 +0300

    Created getters and setters for the employee class

--- src/Employee.java
+++ src/Employee.java
@@ -6,4 +6,44 @@ public class Employee {
     private LocalDateTime joiningDate;
     private String role;
     private double projectProgress;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDateTime getJoiningDate() {
+        return joiningDate;
+    }
+
+    public void setJoiningDate(LocalDateTime joiningDate) {
+        this.joiningDate = joiningDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public double getProjectProgress() {
+        return projectProgress;
+    }
+
+    public void setProjectProgress(double projectProgress) {
+        this.projectProgress = projectProgress;
+    }
 }
