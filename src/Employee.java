27aeeccf5481de64ee1ea283a953dc630c9acce9
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-02-16 22:39:22 +0300

    Created the attributes of Employee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,9 @@
-public class Employee {
+import java.time.LocalDateTime;
 
+public class Employee {
+    private String employeeName;
+    private double salary;
+    private LocalDateTime joiningDate;
+    private String role;
+    private double projectProgress;
 }
