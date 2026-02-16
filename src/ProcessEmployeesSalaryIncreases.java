7504d9671ec6e80746a0e0c2b6fca137286403b1
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-02-17 01:33:47 +0300

    added the salary incraese logic

--- src/ProcessEmployeesSalaryIncreases.java
+++ src/ProcessEmployeesSalaryIncreases.java
@@ -12,9 +12,19 @@ public class ProcessEmployeesSalaryIncreases {
     }
 
     private static void updateSalaryWithIncrease(Employee employee){
+        double oldSalary = employee.getSalary();
+        double newSalary = employee.getSalary();
         // run the salary increase logic here
         if(employee.getProjectProgress() >= 0.6){
+            newSalary = oldSalary + ( oldSalary * Math.floor(employee.getWorkingYears()) * 0.02 );
 
+            if(employee.getRole().equalsIgnoreCase("director")){
+                newSalary = oldSalary + ( oldSalary * 0.05 );
+            }else if(employee.getRole().equalsIgnoreCase("manager")){
+                newSalary = oldSalary + ( oldSalary * 0.02 );
+            }else if(employee.getRole().equalsIgnoreCase("employee")){
+                newSalary = oldSalary + ( oldSalary * 0.01 );
+            }
         }
     }
 }
