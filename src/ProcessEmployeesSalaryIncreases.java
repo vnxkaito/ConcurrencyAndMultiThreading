62d325d18afa258c74567ecd4b53e320c09d81ae
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-02-17 01:55:37 +0300

    updated salary code

--- src/ProcessEmployeesSalaryIncreases.java
+++ src/ProcessEmployeesSalaryIncreases.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicReference;
@@ -7,27 +8,39 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ProcessEmployeesSalaryIncreases {
+    EmployeeCsvIO employeeCsvIO = new EmployeeCsvIO(Path.of("data\\Employees.csv"));
     private static final Semaphore sem = new Semaphore(8);
     private static final Lock salaryLock = new ReentrantLock();
 
     // read all employees
     // pass each employee to the method processSalaryIncrease. Method should save the employee after finishing.
 
-    public void processSalaryIncrease(Employee employee) throws IOException {
-        List<Employee> employees = new EmployeeCsvIO(Path.of("data\\Employees.csv")).readAll();
-        employees.forEach(emp -> {
-            try{
-                sem.acquire();
-                updateSalaryWithIncrease(emp);
-            } catch (InterruptedException ex){
-                Thread.currentThread().interrupt();
-            } finally {
-                sem.release();
+    public void processAllEmployees() throws IOException {
+        List<Employee> employees = employeeCsvIO.readAll();
+        ProcessEmployeesSalaryIncreases process = new ProcessEmployeesSalaryIncreases();
+        employees.forEach(employee -> {
+            try {
+                process.processSalaryIncrease(employee);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         });
     }
 
-    private static void updateSalaryWithIncrease(Employee employee){
+    public void processSalaryIncrease(Employee emp) throws IOException {
+        try{
+            sem.acquire();
+            emp = updateSalaryWithIncrease(emp);
+            employeeCsvIO.update(emp);
+
+        } catch (InterruptedException ex){
+            Thread.currentThread().interrupt();
+        } finally {
+            sem.release();
+        }
+    }
+
+    private static Employee updateSalaryWithIncrease(Employee employee){
         final double oldSalary = employee.getSalary();
 
         AtomicReference<Double> newSalary = new AtomicReference<>(oldSalary);
@@ -44,5 +57,8 @@ public class ProcessEmployeesSalaryIncreases {
                 newSalary.set(oldSalary + ( oldSalary * 0.01 ));
             }
         }
+
+        employee.setSalary(newSalary.get());
+        return employee;
     }
 }
